b730ab3495aca11a4f1a77430c551a2ce2754164
Sam Morrison <sorrison@gmail.com>
2019-09-26 14:09:39 +1000

    Set version to 0.2.0
    Change-Id: Icd662080400a87c969d7335c888117429950df46

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ entry_points = {
 
 setup(
     name='placementclient',
-    version='0.1.0',
+    version='0.2.0',
     description=('Client for the Placement API'),
     author='Sam Morrison',
     author_email='sorrison@gmail.com',
