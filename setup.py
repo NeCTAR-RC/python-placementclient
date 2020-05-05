261338c96652aec5cda44e18563e65570a9f5fa8
Rocky <shi.yan@unimelb.edu.au>
2020-05-05 11:58:33 +1000

    Use pbr for requirements
    Change-Id: I040fe6db66bae7760a7919dc2570fa2534624a3a

--- setup.py
+++ setup.py
@@ -2,23 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import setuptools
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
+from pbr.packaging import parse_requirements
 
-requirements = parse_requirements("requirements.txt", session=False)
 
 entry_points = {
 }
 
 
-setup(
+setuptools.setup(
     name='placementclient',
     version='0.2.0',
     description=('Client for the Placement API'),
@@ -29,7 +22,7 @@ setup(
         'placementclient',
     ],
     include_package_data=True,
-    install_requires=[str(r.req) for r in requirements],
+    install_requires=parse_requirements(),
     license="Apache",
     zip_safe=False,
     classifiers=(
